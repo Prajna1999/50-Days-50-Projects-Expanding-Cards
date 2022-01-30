f85d2bd12aec3230e7b8d072ba0a246d8234b402
Prajna1999 <gituprajna20@gmail.com>
2022-01-30 13:11:11 +0530

    Add: Event Listeners and h3 transition effects

--- script.js
+++ script.js
@@ -2,5 +2,19 @@
 
 const panels=document.querySelectorAll(".panel");
 
-// Adding event listener
-panels.addEventListener()
+// Adding event listener to each of the panels to loop through.
+panels.forEach(panel=>{
+    panel.addEventListener('click', ()=>{
+        removeActiveClasses();
+        panel.classList.add('active');
+    })
+
+})
+
+// removing act{ive classes from other panels.
+function removeActiveClasses(){
+
+    panels.forEach((panel)=>{
+        panel.classList.remove('active');
+    });
+}
